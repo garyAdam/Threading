e2d6c920dbaaef213941f4e7797a2db2f41da455
garyAdam <43093216+garyAdam@users.noreply.github.com>
2019-06-26 15:27:30 +0200

    Task 2 done

---
+++ SingleInstance/SingleInstance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Mutex mutex = null;
+            const string name = "RUNMEONLYONCE";
+            while (true)
+            {
+                try
+                {
+                    mutex = Mutex.OpenExisting(name);
+                    mutex.Close();
+                    System.Environment.Exit(0);
+                }
+                catch (WaitHandleCannotBeOpenedException)
+                {
+                    mutex = new Mutex(true, name);
+                }
+            }
+        }
+    }
+}
