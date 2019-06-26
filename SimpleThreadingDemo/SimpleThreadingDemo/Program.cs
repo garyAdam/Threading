dc457fa18a4a790160c63b7b6ebd019f35c998a0
garyAdam <43093216+garyAdam@users.noreply.github.com>
2019-06-26 15:12:04 +0200

    Task 1 done

---
+++ SimpleThreadingDemo/SimpleThreadingDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace SimpleThreadingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ThreadStart threadStart = new ThreadStart(Counting);
+            Thread thread = new Thread(threadStart);
+            Thread thread2 = new Thread(threadStart);
+            thread.Start();
+            thread2.Start();
+            thread.Join();
+            thread2.Join();
+        }
+
+        static void Counting()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i} {Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
