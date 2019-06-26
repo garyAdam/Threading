6c0f1c461eb869e393889ab62b824c8ad61914cd
garyAdam <43093216+garyAdam@users.noreply.github.com>
2019-06-26 15:56:36 +0200

    Task 3 done

---
+++ ThreadPoolDemo/ThreadPoolDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            WaitCallback waitCallback = new WaitCallback(ShowMyText);
+            ThreadPool.QueueUserWorkItem(waitCallback,"lol ");
+            ThreadPool.QueueUserWorkItem(waitCallback,"lol2 ");
+            ThreadPool.QueueUserWorkItem(waitCallback,"lol3 ");
+            ThreadPool.QueueUserWorkItem(waitCallback,"lol4 ");
+            Thread.Sleep(1000);
+        }
+
+
+        static void ShowMyText(Object state)
+        {
+            string str = state as string;
+            str += Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine(str);
+        }
+    }
+}
